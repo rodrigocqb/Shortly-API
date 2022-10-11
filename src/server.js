339c4d84eb0abdcb9bade45e91237a4bf074f791
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-11 15:48:14 -0300

    Base project structure

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import app from "./index.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server listening on port ${process.env.PORT}!`)
+);
